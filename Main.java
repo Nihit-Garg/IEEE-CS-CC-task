1a6a6563e1c990cb56f7801ecbaf97d9391218fc
NIHIT GARG <nihitgarg@NIHITs-MacBook.local>
2025-03-07 23:33:44 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list= new LinkedList();
+        list.insertFirst(3);
+        list.insertFirst(120);
+        list.insertFirst(90);
+        list.insertFirst(1);
+
+        list.Display();
+    }
+
+}
